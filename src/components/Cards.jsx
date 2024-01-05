fab1b07a7fc036867a3959296bc5c2a7c1d1204b
karlis02 <karli.02@hotmail.com>
2024-01-04 18:26:24 -0600

    ssd

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -22,6 +22,40 @@ const Cards = () => {
     </div>
     </div>
     <div className="flex flex-col justify-center items-center max-w-2xl mx-auto my-8">
+    <img className="w-xl rounded-t-2xl object-cover object-center" src="CCNA1.png" alt='' />
+    <div className="w-96 md:w-96 bg-white mt-0 shadow-lg rounded-lg overflow-hidden">
+    <div className="py-2 text-center font-bold uppercase tracking-wide text-gray-800 m-2">Certificación de CCNA 1</div>
+    <div className="flex items-center justify-between py-2 px-3 bg-gray-400">
+        <h1 className="text-gray-800 font-bold ">CCNA</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-world animate-spin" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <line x1="3.6" y1="9" x2="20.4" y2="9" />
+            <line x1="3.6" y1="15" x2="20.4" y2="15" />
+            <path d="M11.5 3a17 17 0 0 0 0 18" />
+            <path d="M12.5 3a17 17 0 0 1 0 18" />
+        </svg>
+    </div>
+    </div>
+    </div>
+    <div className="flex flex-col justify-center items-center max-w-2xl mx-auto my-8">
+    <img className="w-xl rounded-t-2xl object-cover object-center" src="CCNA2.png" alt='' />
+    <div className="w-96 md:w-96 bg-white mt-0 shadow-lg rounded-lg overflow-hidden">
+    <div className="py-2 text-center font-bold uppercase tracking-wide text-gray-800 m-2">Certificación de CCNA 2</div>
+    <div className="flex items-center justify-between py-2 px-3 bg-gray-400">
+        <h1 className="text-gray-800 font-bold ">CCNA</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-world animate-spin" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <line x1="3.6" y1="9" x2="20.4" y2="9" />
+            <line x1="3.6" y1="15" x2="20.4" y2="15" />
+            <path d="M11.5 3a17 17 0 0 0 0 18" />
+            <path d="M12.5 3a17 17 0 0 1 0 18" />
+        </svg>
+    </div>
+    </div>
+    </div>
+    <div className="flex flex-col justify-center items-center max-w-2xl mx-auto my-8">
     <img className="w-xl rounded-t-2xl object-cover object-center" src="certificacion2.jpg" alt='' />
     <div className="w-96 md:w-96 bg-white mt-0 shadow-lg rounded-lg overflow-hidden">
     <div className="py-2 text-center font-bold uppercase tracking-wide text-gray-800 m-2">Certificación en charlas de Telecomunicaciones</div>
